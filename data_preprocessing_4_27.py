1e5edefad6702d694697a7889b9411aecb32c5e1
Ricky513125 <lilingyu513125@163.com>
2025-04-27 17:52:42 +0800

    data process

--- data_preprocessing_4_27.py
+++ data_preprocessing_4_27.py
@@ -5,6 +5,8 @@ import numpy as np
 new_user_class_level 和广告的brand有nan值影响
 发现链接没有指定left
 
+4.27 -2 发现有数据集合并后， sample表里有没出现过的userid，
+重新合并，将没有在表里的设为冷启动
 """
 
 def load_and_preprocess():
@@ -20,6 +22,10 @@ def load_and_preprocess():
     user_profile = user_profile.rename({'userid': 'user_id'}, axis=1)
     u = raw_sample[~raw_sample['user_id'].isin(user_profile.user_id)]
     print(u)
+    u.to_parquet('data/raw_sample_train_cold_user.parquet', u)
+    a = raw_sample[~raw_sample['adgroup_id'].isin(ad_feature['adgroup_id'])]
+    a.to_parquet('data/raw_sample_train_cold_ad.parquet', a)
+    raw_sample = raw_sample[raw_sample['adgroup_id'].isin(ad_feature['adgroup_id']) & raw_sample['user_id'].isin(user_profile.user_id)]
     data = pd.merge(raw_sample, user_profile, how='left', on='user_id')
     data = pd.merge(data, ad_feature, how='left', on='adgroup_id')
 
@@ -49,6 +55,15 @@ def load_and_preprocess_test():
     # 合并数据
     raw_sample = raw_sample.rename({'user': 'user_id'}, axis=1)
     user_profile = user_profile.rename({'userid': 'user_id'}, axis=1)
+
+    u = raw_sample[~raw_sample['user_id'].isin(user_profile.user_id)]
+    print(u)
+    u.to_parquet('data/raw_sample_test_cold_user.parquet', u)
+    a = raw_sample[~raw_sample['adgroup_id'].isin(ad_feature['adgroup_id'])]
+    a.to_parquet('data/raw_sample_test_cold_ad.parquet', a)
+    raw_sample = raw_sample[
+        raw_sample['adgroup_id'].isin(ad_feature['adgroup_id']) & raw_sample['user_id'].isin(user_profile.user_id)]
+
     data = pd.merge(raw_sample, user_profile, how='left', on='user_id')
     data = pd.merge(data, ad_feature, how='left', on='adgroup_id')
 
@@ -70,6 +85,6 @@ def load_and_preprocess_test():
 
 if __name__ == "__main__":
     data = load_and_preprocess()
-    data.to_parquet('data/processed_data2.parquet')  # 保存预处理数据
+    data.to_parquet('data/processed_data3.parquet')  # 保存预处理数据
     d = load_and_preprocess_test()
-    d.to_parquet('data/processed_data_test2.parquet')
+    d.to_parquet('data/processed_data_test3.parquet')
